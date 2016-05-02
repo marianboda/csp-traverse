cb9179c48a6db7b28fddf21fb3dc6aed833eccca
Marian Boda <marianboda@gmail.com>
2016-05-02 15:00:24 +0200

    Removed plain-text parser from mediainfo

--- src/mediainfo.js
+++ src/mediainfo.js
@@ -1,5 +1,4 @@
 const exec = require('child_process').exec
-const _ = require('lodash')
 const xml2js = require('xml2js')
 const xmlParse = xml2js.parseString
 
@@ -18,35 +17,6 @@ function xmlInfo(filePath, cb){
       }, {})
       cb(null, ob)
     })
-  })
-}
-
-function minfo(filePath, cb){
-  exec(`mediainfo ${filePath}`, (err, res) => {
-    if (err) return cb(err)
-
-    let sections = res.split("\n\n")
-    let mediaObject = sections.map((i) => {
-      let lines = i.split("\n")
-      let title = _.head(lines)
-      let itemLines = _.tail(lines)
-      let items = itemLines.map(j => {
-        let item = j.split(':').map(str => str.trim())
-        return [_.head(item), _.tail(item).join(':')]
-      }).reduce((acc, el) => {
-        let newOb = {}
-        newOb[el[0]] = el[1]
-        return Object.assign({}, acc, newOb)
-      }, {})
-      return [title, items]
-    }).reduce((acc, el) => {
-      if (el[0] == '' || el[1] == '')
-        return acc
-      let newOb = {}
-      newOb[el[0]] = el[1]
-      return Object.assign({}, acc, newOb)
-    }, {})
-    return cb(null, res)
   })
 }
 
